34fd9fd77fce4a568eb5ba899f19b24d72357da4
Abdullah.m <abmeshhen@hotmail.com>
2023-02-17 22:40:39 +0300

    Add some comments

--- Investment.java
+++ Investment.java
@@ -1,13 +1,20 @@
 import java.util.*;
 public class Investment {
+    // Here we make a method that calculate the future investment
     public static double futureInvestmentValue(double InvestmentAmount,double monthlyInterestRate,int years){
         double sum=0;
         int i=1;
         double InvestValue=0;
+        // Note that the we use the i in the rule that means in the loop it will calculate the from 1 to the number of year 
+        // that you enter
+        
+
         for(i=1;i<=years;i++){
+            // The rule to calculate the values
              InvestValue= InvestmentAmount*(Math.pow(1+((monthlyInterestRate/100)/12), 12*(double)i));
             System.out.print(i+"  "+InvestValue);
             System.out.println();
+            // Here it will add the investment
             sum+=InvestValue;
         }
         return InvestValue;
